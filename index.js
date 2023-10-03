da947c04630b1a6133bbc3345f0adf993fe01791
ephraimnjue <ephraim.njagi@student.moringaschool.com>
2023-10-03 09:05:59 +0300

    update scuber

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 function scuberGreetingForFeet(){
   // Write your code here!
 }
+if (distance <= 400)
+return 'This one is on me!';
+
+
+else if (distance > 2000 && distance < 2500) 
+return 'I will gladly take your thirty bucks.';
+
+else if (distance > 2500) 
+return 'No can do.';
+
+}
 
 function ternaryCheckCity(){
   // Write your code here!
